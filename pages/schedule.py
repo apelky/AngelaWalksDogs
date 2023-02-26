e0023c1e8be606c57985beefef847160913212bd
Angela Pelky <angelapelky@dyn-10-108-127-234.wless.uoregon.edu>
2023-02-26 14:11:08 -0800

    New notes added to schedule + cleaned up functions

--- pages/schedule.py
+++ pages/schedule.py
@@ -1,3 +1,19 @@
+'''
+schedule.py last edited on ?.
+
+Angela Pelky
+
+This acts as a page of my web app that prompts the user to schedule a service and adds it to my calendar.
+Please note that this is a prototype, and some of the questions prompted such as address and key code could be compromisable.
+Upon release of this app please ensure that all security measures have been met. Furthermore, this schedule does not include the need
+for driving time between bookings. It would also be nice if I could find a way to make Google Calendar look nicer. This program calls 
+upon the helper progam Authenticator.py to build the Google Calendar and insert an event into it.
+
+----------------------------------------
+
+schedule.py uses Python 3.10
+'''
+
 import streamlit as st
 import datetime
 import streamlit.components.v1 as components
@@ -6,39 +22,17 @@ from Authenticator import main_auth
 
 DEFAULT = 'America/Los_Angeles'
 
-# a dictionary where the username is the key and the value is a dictionary with their event details
 # TODO: create a dictionary of dictionaries or use session state?
+# a dictionary where the username is the key and the value is a dictionary with their event details
 # events = {}
+
 # event dictionary is sent to Google Calendar
 event = {}
-test_event = {
-  'summary': 'Google I/O 2015',
-  'location': '800 Howard St., San Francisco, CA 94103',
-  'description': 'A chance to hear more about Google\'s developer products.',
-  'start': {
-    'dateTime': '2015-05-28T09:00:00-07:00',
-    'timeZone': 'America/Los_Angeles',
-  },
-  'end': {
-    'dateTime': '2015-05-28T17:00:00-07:00',
-    'timeZone': 'America/Los_Angeles',
-  },
-  'recurrence': [
-    'RRULE:FREQ=DAILY;COUNT=2'
-  ],
-  'attendees': [
-    {'email': 'lpage@example.com'},
-    {'email': 'sbrin@example.com'},
-  ],
-  'reminders': {
-    'useDefault': False,
-    'overrides': [
-      {'method': 'email', 'minutes': 24 * 60},
-      {'method': 'popup', 'minutes': 10},
-    ],
-  },
-}
 
+
+# Helper function that asks the user how long they'd like their service to be.
+# Parameters: None
+# Return: Duration(string)
 def duration():
     duration = st.selectbox(
         'Select a duration',
@@ -46,6 +40,10 @@ def duration():
     )
     return duration[:2]
 
+
+# Helper function that asks the user which day they'd like their service to be on.
+# Parameters: None
+# Return: d(datetime.date)
 def schedule():
     d = st.date_input(
         'Select a day',
@@ -53,16 +51,13 @@ def schedule():
         )
     return d
 
+
+# Helper function that asks the user what time they'd like their service to occur at.
+# Parameters: None
+# Return: t(datetime.time)
 def time():
     t = st.time_input('See calendar, select a time when I am not already "busy"', datetime.time(9, 00))
     return t
-
-def schedule_builder():
-    r_duration = duration()
-    r_day = schedule()
-    r_time = time()
-
-    return r_duration, r_day, r_time
 
 # embed calendar for viewing within my app
 components.html(
@@ -72,6 +67,7 @@ components.html(
     height=700,
 )
 
+# prompt user for various information
 option = st.selectbox(
     'What kind of service would you like to book?',
     ('Walk', 'Drop-In')
@@ -93,34 +89,41 @@ get_notes = st.text_input(
   'Enter any access instructions I would need to get inside the home. For example: the access code is xyz or I left the key under the mat!'
 )
 
-if option == 'Walk':
-    walk_duration, walk_day, walk_time = schedule_builder()
-    run = st.button('Done')
-    
-    # must convert it to a full datetime object before adding walk duration
-    datetime_obj = (datetime.datetime.combine(walk_day, walk_time))
-    end_time = (datetime_obj + datetime.timedelta(minutes=int(walk_duration)))
-    datetime_obj = datetime_obj.strftime('%Y-%m-%dT%H:%M:%S-08:00')
-    if datetime_obj == test_event['start']['dateTime']:
-        print("Exactly!")
-    end_time = end_time.strftime('%Y-%m-%dT%H:%M:%S-08:00')
-    print(end_time)
 
-    event['summary'] = 'Walk with '+get_pet
+# Helper function that builds the event dictionary in preparation for calling the Google Calendar API.
+# Parameters: w_or_drop(str)
+# Return: event(dict)
+def schedule_builder(w_or_drop):
+    r_duration = duration()
+    r_day = schedule()
+    r_time = time()
+
+    # must convert it to a full datetime object before adding the duration
+    datetime_obj = (datetime.datetime.combine(r_day, r_time))
+    # add the duration to the start time to create an end time
+    end_time = (datetime_obj + datetime.timedelta(minutes=int(r_duration)))
+    # convert our start and end time to match Google Calendar requirements
+    datetime_obj = datetime_obj.strftime('%Y-%m-%dT%H:%M:%S-08:00')
+    end_time = end_time.strftime('%Y-%m-%dT%H:%M:%S-08:00')
+
+    event['summary'] = w_or_drop+get_pet
     event['location'] = get_location
     event['description'] = get_notes
     event['start'] = {'dateTime': datetime_obj, 'timeZone':DEFAULT}
-    if(event['start']['dateTime'] != test_event['start']['dateTime']):
-        print("fml")
-        print(event['start']['dateTime'])
-        print(test_event['start']['dateTime'])
     event['end'] = {'dateTime': end_time, 'timeZone':DEFAULT}
+
+    return event
+
+if option == 'Walk':
+    event = schedule_builder('Walk with ')
+    run = st.button('Done')
+
     if run:
-        #start = test_event['start']
-        #start = start['dateTime']
-        #print(type(start))
         main_auth(event)
 
 if option == 'Drop-In':
-    #TODO
-    drop_duration, drop_day, drop_time = schedule_builder()
+    event = schedule_builder('Drop-In with ')
+    run = st.button('Done')
+
+    if run:
+        main_auth(event)
